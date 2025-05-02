9562f8f610f504a57c501da7a7799944d8192907
LuBeneventi <lu.beneventi@duocuc.cl>
2025-05-02 13:09:34 -0400

    Modelo Cliente

---
+++ src/main/java/com/Registro/MSCliente/model/cliente.java
@@ -0,0 +1,18 @@
+package com.Registro.MSCliente.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class cliente {
+    private int idCliente;
+    private String nomCliente;
+    private String apCliente;
+    private String correoCliente;
+    private String dirCliente;
+    private estadoCliente estado;
+}
---
+++ src/main/java/com/Registro/MSCliente/model/estadoCliente.java
@@ -0,0 +1,5 @@
+package com.Registro.MSCliente.model;
+
+public enum estadoCliente {
+    ACTIVO, INACTIVO
+}
